3871b4aaf734af5560bd536ea44fc14f1665e1c8
Rushikesh Mote <moterushikesh39@gmail.com>
2025-10-13 23:54:36 +0530

    learing data first approach

---
+++ WebApplication1DatabaseApproach/WebApplication1DatabaseApproach/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1DatabaseApproach.Models;
+
+namespace WebApplication1DatabaseApproach.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        restroEntities r1 = new restroEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var x=r1.customers.ToList();
+            return View();
+        }
+    }
+}

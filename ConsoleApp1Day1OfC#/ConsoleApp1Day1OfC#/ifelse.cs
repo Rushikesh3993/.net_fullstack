9edeb480bfd4ae5dce88d7609c5c4881844b3400
Rushikesh Mote <moterushikesh39@gmail.com>
2025-11-06 22:29:35 +0530

    learn the if else and nestedif else, switch case in c#

---
+++ ConsoleApp1Day1OfC#/ConsoleApp1Day1OfC#/ifelse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Day1OfC_
+{
+    internal class ifelse
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Conditional Statements");
+            Console.WriteLine("Enter Your First Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Your second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if(a == b)
+            {
+                Console.WriteLine("This number is equal to each other");
+            }
+            else if(a > b)
+            {
+                Console.WriteLine("First number is greater than seconnd");
+            }
+            else if(b > a)
+            {
+                Console.WriteLine("Second Number is grater than first");
+            }
+            else if(a < b)
+            {
+                Console.WriteLine(" first number is smaller than second");
+            }
+            else if (b < a)
+            {
+                Console.WriteLine(" second number is smaller than first");
+            }
+            else
+            {
+                Console.WriteLine("invalid integers");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1Day1OfC#/ConsoleApp1Day1OfC#/nestedifelse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Day1OfC_
+{
+    internal class nestedifelse
+    {
+        public static void Main(string[] args)
+        {
+            //int raju = 75, sanju = 90, mukesh = 20;
+
+            //if (raju > sanju)
+            //{
+            //    Console.WriteLine("Raju ag is grater than sanju");
+            //}
+            //else if (sanju > mukesh)
+            //{
+            //    Console.WriteLine("Sanju age is grater than mukesh");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("mukesh age is greater than all");
+            //}
+
+            Console.WriteLine("Voting application");
+            int age = 19;
+            bool is_voting = true;
+
+            if(age > 18)
+            {
+                if (is_voting == true)
+                {
+                    Console.WriteLine("Your are Eligible for voting");
+                }
+                else
+                {
+                    Console.WriteLine("You Are not eligible for voting");
+                }
+            }
+            else
+            {
+                Console.WriteLine("INvalid Age");
+            }
+        }
+    }
+}
